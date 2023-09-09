5ec77ea5ce2fcc2de23ffd4a96700eb38468952c
Sanjar <144442005+Seijun7777@users.noreply.github.com>
2023-09-10 00:40:23 +0500

    Some changes

--- Not the first code.py
+++ Not the first code.py
@@ -1,1 +1,13 @@
 print("What's up World?")
+
+dict_1 = {"Family":
+        {"Dad": "Kuvanish",
+         "Mom": "Salima",
+         "Sister": "Sevara",
+         "Me": "Sanjar"},
+          "Age":
+        {"Dad": 1975,
+         "Mom": 1978,
+         "Sister": 2003,
+         "Me": 2007}}
+print(f"My dad's name is {dict_1['Family']['Dad']} and he is {dict_1['Age']['Dad']}")
